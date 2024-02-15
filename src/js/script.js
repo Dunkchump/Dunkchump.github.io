84056a99e04928beb3b2ec21e00cd90e605472dd
Dunkchump <valik.korolev6@gmail.com>
2024-02-15 20:46:36 +0200

    Catalog ADD

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,21 @@
-alert (1)
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></img></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></img></button>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                  dots: true,
+                  arrows: false
+                  
+                }
+              }
+        ]
+    });
+  });
+
+
+  
